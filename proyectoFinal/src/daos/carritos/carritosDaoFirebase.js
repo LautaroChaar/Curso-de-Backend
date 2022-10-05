f0516d8374e9dff0b290173f5c2aa57725d69bd1
Lautaro Chaar <lautarochaar@hotmail.com>
2022-10-05 19:53:02 -0300

    eliminando carpetas

--- proyectoFinal/src/daos/carritos/carritosDaoFirebase.js
+++
@@ -1,9 +0,0 @@
-import ContenedorFirebase from '../../containers/contenedorFirebase.js';
-
-class CarritosDaoFirebase extends ContenedorFirebase {
-    constructor() {
-        super('productos', 'carritos');
-    }
-}
-
-export default CarritosDaoFirebase;
--- proyectoFinal/src/daos/productos/productosDaoFirebase.js
+++
@@ -1,9 +0,0 @@
-import ContenedorFirebase from '../../containers/contenedorFirebase.js';
-
-class ProductosDaoFirebase extends ContenedorFirebase {
-    constructor() {
-        super('productos', 'carritos');
-    }
-}
-
-export default ProductosDaoFirebase;
