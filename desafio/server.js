a8c744f0f700243e5a5239d487384f519989dbf6
Lautaro Chaar <lautarochaar@hotmail.com>
2022-11-21 22:21:05 -0300

    Port heroku

--- desafio/server.js
+++ desafio/server.js
@@ -71,9 +71,8 @@ minimist([], options);
 
 const CPU_CORES = os.cpus().length;
 const MODO = args.modo || args.m || options.default.modo;
-const PORT = process.env.PORT
+const PORT =  parseInt(process.env.PORT) || parseInt(process.argv[2]) || args.port || args.p || options.default.port ;
 
-// || parseInt(process.argv[2]) || args.port || args.p || options.default.port ;
 
 if (cluster.isPrimary && MODO == 'CLUSTER') {
 
