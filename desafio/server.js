3a82cdca41d341b928be41c1b933b5c62ecbfa9d
Lautaro Chaar <lautarochaar@hotmail.com>
2022-11-23 21:42:42 -0300

    merge cloud

--- desafio/server.js
+++ desafio/server.js
@@ -71,7 +71,7 @@ minimist([], options);
 
 const CPU_CORES = os.cpus().length;
 const MODO = args.modo || args.m || options.default.modo;
-const PORT =  process.env.PORT;
+const PORT =  process.env.PORT || 8080;
 
 // parseInt(process.argv[2]) || args.port || args.p || options.default.port ;
 
