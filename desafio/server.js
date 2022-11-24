11bab991ca0e0bc8e91600a66f8119697f3929ab
Lautaro Chaar <lautarochaar@hotmail.com>
2022-11-23 21:15:37 -0300

    Heroku

--- desafio/server.js
+++ desafio/server.js
@@ -71,7 +71,9 @@ minimist([], options);
 
 const CPU_CORES = os.cpus().length;
 const MODO = args.modo || args.m || options.default.modo;
-const PORT =  process.env.PORT || parseInt(process.argv[2]) || args.port || args.p || options.default.port ;
+const PORT =  process.env.PORT;
+
+// parseInt(process.argv[2]) || args.port || args.p || options.default.port ;
 
 
 if (cluster.isPrimary && MODO == 'CLUSTER') {
